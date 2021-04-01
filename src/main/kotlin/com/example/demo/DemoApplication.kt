467dd872c669e3058b9fcbced46fb143495bcc72
dragonfly95 <dQzNKCyAgf43e3B>
2021-04-01 13:48:18 +0900

    2020-04-01 kotlin 업데이트

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,6 +2,10 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class DemoApplication
@@ -11,3 +15,18 @@ fun main(args: Array<String>) {
 }
 
 
+@RestController
+@RequestMapping("/api/article")
+class HelloController(private val repository: ArticleRepository) {
+
+//	@RequestMapping("/test")
+//	fun hello(): ResponseEntity<List<Int>> {
+//		val list = listOf(1, 2, 3, 4, 5);
+//		return ResponseEntity.ok().body(list)
+//	}
+
+
+	@GetMapping("")
+	fun findAll() = repository.findAllByOrderByAddedAtDesc()
+
+}
